ab70c935d8f929e7b7961f81c42c7396607333f2
Marcus <marcus@hardt-it.de>
2025-09-05 11:57:14 +0200

    improve instructions

--- setup-env.sh
+++ setup-env.sh
@@ -5,4 +5,6 @@ git submodule update
 # Apply patch to allow parsing without core schema
 patch -p1 < scimschema.patch
 export PYTHONPATH=`pwd`/scimschema
+
+echo -e "Please export the PYTHONPATH like this:\n"
 echo "export PYTHONPATH=`pwd`/scimschema"
